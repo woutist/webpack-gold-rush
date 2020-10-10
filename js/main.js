e995f29bec190cc6b37c2b212788477e3134407e
Aleksander Nyczyk <williamoutist@gmail.com>
2020-10-11 00:00:55 +0200

    Test

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import '../scss/index.scss';
 import {setCookies, removeCookies, getCookies} from './cookies';
 import {detectionDevice, detectionWEBGL, isEven, inArrayObject, inArray, isExists, removeA, randomBetween, randomArray, triggerKeyboardEvent} from './functions';
 
-console.log("test3");
+console.log("test4");
 
 // for IE11
 Number.isInteger = Number.isInteger || function(value) {
